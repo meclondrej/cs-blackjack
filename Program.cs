ef9d7a75f2040d4e45d772e8cc7c9d88cd233c3d
Ondrej Mecl <meclondrej@protonmail.com>
2023-01-18 21:07:17 +0100

    Added the logic for setting up multiple players
    If passed correctly, adds the according amounts of players to the players array.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 ﻿class Program {
     static void Main(string[] args) {
-        
+        List<Player> players = new List<Player>();
+        if (args.Length != 0) {
+            int count;
+            if (int.TryParse(args[0], out count)) {
+                for (int i = 0; i < count; i++) {
+                    players.Add(new Player());
+                }
+            } else {
+                Console.WriteLine("unable to convert: invalid format");
+                return;
+            }
+        } else {
+            Console.WriteLine("no player count argument passed: falling back to 1 player");
+        }
     }
 }
