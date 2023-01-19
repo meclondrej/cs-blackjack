c963b11fed57d6270bcfe7229b779d8b6eb45367
Ondrej Mecl <meclondrej@protonmail.com>
2023-01-19 16:47:58 +0100

    Added the player dialog loop

--- Program.cs
+++ Program.cs
@@ -15,5 +15,30 @@
             Console.WriteLine("no player count argument passed: falling back to 1 player");
             players.Add(new Player());
         }
+        Console.WriteLine("C# Blackjack");
+        for (int i = 0; i < players.Count; i++) {
+            Console.WriteLine("");
+            bool loop = true;
+            while (loop) {    
+                Console.WriteLine(players[i].getFormattedHand(i + 1));
+                if (players[i].isBJ()) {
+                    Console.WriteLine("Player " + (i + 1) + " BLACKJACKED!");
+                    players[i].eval = false;
+                    loop = false;
+                } else if (players[i].isBusted()) {
+                    Console.WriteLine("Player " + (i + 1) + " BUSTED!");
+                    players[i].eval = false;
+                    loop = false;
+                } else {
+                    Console.WriteLine("hit/stand [h/s]: ");
+                    string? choice = Console.ReadLine();
+                    if (choice == "h") {
+                        players[i].hand.Add(new Card());
+                    } else if (choice == "s") {
+                        loop = false;
+                    }
+                }
+            }
+        }
     }
 }
