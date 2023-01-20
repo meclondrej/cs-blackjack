f01cce6a1446540a3b9a2bc5cd3e55afcefff358
Ondrej Mecl <meclondrej@protonmail.com>
2023-01-20 18:57:38 +0100

    Added the getFormattedHand function to the dealer

--- Dealer.cs
+++ Dealer.cs
@@ -7,4 +7,12 @@ class Dealer : Player {
             this.hand.Add(new Card());
         }
     }
+    public string getFormattedHand() {
+        string msg = "Dealer's hand: ";
+        foreach (Card card in this.hand) {
+            msg = msg + Card.ResolveValue(card.value) + " ";
+        }
+        msg = msg + "(Total: " + this.getTotal() + ")";
+        return msg;
+    }
 }
